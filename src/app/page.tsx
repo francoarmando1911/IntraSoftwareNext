4b5f02b7dd655a4351d219754e257cc0337138fa
Franco Armando <francoarmando1911@gmail.com>
2025-02-05 09:41:05 -0300

    actualizacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>prueba</h1>
+    <div className="">
+      <h1 className="font-bold text-red-700 text-6xl pt-10">Prueba</h1>
     </div>
   );
 }
